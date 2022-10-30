aba9d5fcb0ab77eb90811f4772a3d57a6ec61a3d
arrbrk1 <acorelado@gmail.com>
2022-10-30 17:33:14 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       {candidates.map((candidate) => (
         <div key={candidate.seq}>
           <h1>{candidate.nm}</h1>
-          <h2>Votos: {candidate.vap}</h2>
+          <h2 style={{ color: "green" }}>Votos: {candidate.vap}</h2>
         </div>
       ))}
     </div>

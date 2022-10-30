f6e7fccb4f194bfb9733f356a70a6718d4cc5576
arrbrk1 <acorelado@gmail.com>
2022-10-30 17:31:50 -0300

    app

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [candidates, setCandidates] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setCandidates(data.cand));
+  });
+
+  return (
+    <div className="App">
+      {candidates.map((candidate) => (
+        <div key={candidate.seq}>
+          <h1>{candidate.nm}</h1>
+          <h2>Votos: {candidate.vap}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
